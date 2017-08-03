d63d476ea84a196a9dbbb193a50cf4a2f8201c16
even713 <contactzyw@hotmail.com>
2017-08-03 14:24:32 +0800

    Provide base class for DT settings.

--- examples/angular-datatable/src/uilaDataTable.js
+++ examples/angular-datatable/src/uilaDataTable.js
@@ -7,4 +7,37 @@ angular.module("uilaDatatable", ['ngSanitize'])
 
 .directive("uilaColumn", UilaColumnDirective.create())
 
+// TODO: combine js to hide variable, create service to get columnformatter, tablehelper etc
 
+// a base class to be used in setting DT options.
+class UilaTableSettingsHelper extends laygoon.util.BaseNgClass {
+	constructor(...injects) {
+		super(...injects);
+
+		// http://legacy.datatables.net/usage/options
+		// http://legacy.datatables.net/usage/callbacks
+		this.dtOptions = {
+			dom: "tr",
+			responsive: {
+				details: {
+					renderer: this.myRenderer.bind(this)
+				}
+			},
+			fnCreatedRow: this.fnCreatedRow.bind(this)
+		}
+	}
+
+	// override dt options so that we can customize dtoptions
+	setOptions(setting) {
+		this.dtOptions = $.extend(this.dtOptions, setting);
+	}
+
+	/* below methods are mapping DT apis and to be overriden */
+	myRenderer (api, rowIdx) {
+
+	}
+
+	fnCreatedRow(nRow, aData, iDataIndex) {
+		console.log(arguments);
+	}
+}
