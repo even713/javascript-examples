db1dcb527a3b55600826389775aacf8ca430dcf6
even713 <contactzyw@hotmail.com>
2017-08-22 15:39:14 +0800

    some update

--- examples/angular-datatable/src/UilaColumnDirective.js
+++ examples/angular-datatable/src/UilaColumnDirective.js
@@ -8,6 +8,16 @@ class UilaColumnDirective extends laygoon.util.BaseDirectiveClass {
 		this.transclude = true;
 		this.template = "<th ng-transclude></th>";
 		this.replace = true;
+
+		this.setScope({
+			// sort function for sorting the column, 
+			// currently it only for make it consist with UI when dynimic update
+			// if we use "data-order" attribute with sortFn, it has issue when cell is canvas
+			// jquery.datatable.js line 6370
+			// because "data-order" is equal to set mData on column and DT will copy cell html and failed to draw the canvas
+			// besides, copy html is unnecessay since the html is already there.
+			sortFn: '=' 
+		});
 	}
 
 	link (scope, element, attr, uilaTable){
@@ -18,7 +28,9 @@ class UilaColumnDirective extends laygoon.util.BaseDirectiveClass {
 		let sType = attr.sType,
 			sClass = attr.sClass,
 			bSortable = attr.bSortable,
-			className = attr.className;
+			className = attr.className,
+			sWidth = attr.sWidth,
+			key = attr.key;
 
 		if(sType != null)
 			settings["sType"] = sType;
@@ -28,7 +40,21 @@ class UilaColumnDirective extends laygoon.util.BaseDirectiveClass {
 			settings["bSortable"] = bSortable == 'true';
 		if(className != null)
 			settings["className"] = className;
+		if(sWidth != null)
+			settings["sWidth"] = sWidth;
 
 		uilaTable.setColumnDef(colIdx, settings);
+
+		let sortFn = scope.sortFn;
+		if(!sortFn && key) {
+			sortFn = function(key){
+				return function(elem){
+					return elem[key];
+				}
+			}(key);
+			//element.attr("sort-fn", true);
+		} 
+
+		uilaTable.setSortFn(colIdx, sortFn);
 	}
 }
--- examples/angular-datatable/src/UilaTableDirective.js
+++ examples/angular-datatable/src/UilaTableDirective.js
@@ -1,3 +1,5 @@
+const DATA_ORDER_PROP = "dataOrder";
+
 class UilaTableFirstDirective extends laygoon.util.BaseDirectiveClass {
 	constructor(...injects) {
 		super(...injects);
@@ -15,18 +17,20 @@ class UilaTableFirstDirective extends laygoon.util.BaseDirectiveClass {
 
 		columns.each(function(idx, col){
 			let key = $(col).attr("key"),
-				formatter = $(col).attr("formatter");		
+				formatter = $(col).attr("formatter"),
+				filter = $(col).attr("filter");
 
-			if(!formatter) {
-				if(!key) {
-					console.error("no key or formatter for table column");
-					return;
-				}
-				tdArray.push("<td>{{ elem."+ key + "}}</td>");
-			} else {
+			if(formatter) {
 				tdArray.push('<td><'+ formatter + ' col-idx="'+ idx +'" row-idx="$index" row-data="elem" ' +
 	                '></'+ formatter +'></td>');
-			}			
+			} else if(filter) {
+				tdArray.push(`<td><span ng-bind-html=elem.`+ key + `|`+ filter +`:elem:"`+ idx +`":$index></td>`);
+			} else if(key){
+				tdArray.push("<td>{{ elem."+ key + "}}</td>");
+			} else {
+				console.error("no key/formatter/filter for table column");
+				return;
+			}		
 		});
 
 		tElem.data("tdArray", tdArray);		
@@ -40,7 +44,7 @@ class UilaTableSecondDirective extends laygoon.util.BaseDirectiveClass {
 		this.restrict = "EA";
 
 		this.scope = {
-			data: '=',
+			sourceData: '=data',
 			dtOptions: '=',
 			detailRenderer: '=',
 			fnCreatedRow: '='
@@ -69,25 +73,35 @@ class UilaTableSecondDirective extends laygoon.util.BaseDirectiveClass {
 	}
 
 	_compile(tElem, tAttrs) {
+		tElem.css("visibility", "hidden");
+
 		let tbodyTr = tElem.find("tbody > tr"),
-			trNgRepeat = 'elem in vm.data track by elem.'+ tAttrs.trackBy,
+			trackBy = tAttrs.trackBy ? "elem." + tAttrs.trackBy : "$index",
+			trNgRepeat = 'elem in vm.data track by '+ trackBy,
 			tdArray = tElem.data("tdArray"),
 			_injects = this;
 
 		tbodyTr.attr("ng-repeat", trNgRepeat);
+		tbodyTr.attr("ng-init", "$last && finished()");
+		tbodyTr.attr("key-idx", "{{"+ trackBy +"}}");
 
 		tdArray.forEach(function(td){
 			tbodyTr.append(td);
 		});
 
+
 		return {
 			post: function(scope, element, attr, ctrl) {
-
 				ctrl.setDTOption(ctrl.dtOptions);
-
-				_injects.$timeout(function(){
-					ctrl.initTable(element);
-				});
+				let trackBy = attr.trackBy;
+				ctrl.setTableKey(trackBy);
+				
+				scope.finished = function(){
+					_injects.$timeout(function(){
+						ctrl.initTable(element);
+						element.css("visibility", "visible");
+					});
+				}
 			}
 		}
 	}
@@ -101,25 +115,94 @@ class UilaTableDirectiveWorker extends laygoon.util.BaseNgClass {
 		this.oldTrs = null; // store the current UI trs element
 		this.dtInstance = null; // a refrence that can access datatable api
 		this.dtColumnDefs = [];
+		this.sortFns = [];
 	}
 
-	get data(){
-		return this._data;
+	// prepare data with data attribute for sorting etc.
+	// prepareData(newData){
+	// 	let _self = this;
+
+	// 	if(!newData)
+	// 		return false;
+
+	// 	newData.forEach(function(d){
+	// 		_self.sortFns.forEach(function(sortfn, idx){
+	// 			if(sortfn) {
+	// 				d[DATA_ORDER_PROP + idx] = sortfn(d, idx);
+	// 			}
+	// 		});
+	// 	});
+	// 	return newData;
+	// }
+
+	get sourceData(){
+		return this._sourceData;
 	}
 
 	// watch data change and update datatable
-	set data(newData){	
+	set sourceData(newData){				
+		this._sourceData = newData;
+		if(!newData){
+			console.log("set data with undefined");
+			return;
+		}
+
 		let _self = this;
-				
 		if(this.dtInstance) {
-			this._data = newData;
+			// this._data = this.prepareData(newData);
+			this.data = this.sortData(newData);
 
 			this.$timeout(function(){
 				_self.updateTable();
 			});
 		} else {
-			this._data = newData;
+			this.data = newData;
 		}
+	}
+
+	setTableKey(key){
+		this.tableKey = key;
+	}
+
+	// sort data with current order
+	sortData(data){
+		if(!this.dtInstance)
+			return;
+
+		let _data = data.slice(0);// copy source data
+		let _self = this;
+		let trs = this.table.find('>tbody').children().not(".child");
+		let resultAry = [];
+		trs.each(function(idx, tr){
+			let keyIdx = $(tr).attr("key-idx");
+			for(let i = 0; i < _data.length; i++) {
+				if(_data[i][_self.tableKey] == keyIdx) {
+					resultAry.push(_data[i]);
+					_data.splice(i, 1);
+					break;
+				}
+			}
+		});
+
+		resultAry = resultAry.concat(data);
+
+		// let _self = this;
+		// let order = this.dtInstance.order()[0];
+		// let col = order[0], dir = order[1];
+		// if(this.sortFns[col]) {
+		// 	let sortFn = this.sortFns[col];
+		// 	data.sort(function(a, b){
+		// 		let aa = sortFn(a, col),
+		// 			bb = sortFn(b, col);
+
+		// 		if(dir == "asc")
+		// 			return ((aa < bb) ? -1 : ((aa > bb) ?  1 : 0));
+		// 		if(dir == "desc")
+		// 			return ((bb < aa) ? -1 : ((bb > aa) ?  1 : 0));
+		// 	});
+		// }
+
+		return resultAry;
 	}
 
 	// set column defination, see aoColumnDefs: http://legacy.datatables.net/usage/columns
@@ -135,11 +218,15 @@ class UilaTableDirectiveWorker extends laygoon.util.BaseNgClass {
 		this.dtOption = $.extend(this.dtOption, settings);
 	}
 
+	setSortFn(targetCol, sortFn) {
+		this.sortFns[targetCol] = sortFn;
+	}
+
 	initTable(table) {
 		let _self = this;
 		this.table = table;
 		// this.oldTrs is for angular produced datatable's trs
-        this.oldTrs = table.find('tbody').children().not(".child");
+        this.oldTrs = table.find('>tbody').children().not(".child");
         
         let tableSettings = {
         	retrieve: true,
@@ -151,9 +238,20 @@ class UilaTableDirectiveWorker extends laygoon.util.BaseNgClass {
         this.dtInstance = $(table).DataTable(tableSettings);
 	}
 
-	updateTable() {
+	updateTable(){
+		// use rows().every() instead when datatable is 1.10.6+
+		let table = this.dtInstance;
+		table.rows().eq(0).each( function ( index ) {
+		    table.row( index ).invalidate(); // update row data		 	
+		} );
+
+		this._updateTable();
+	}
+
+	_updateTable() {
+
 		// Get the angular trs before dt's draw
-        let trs = $(this.table).find('tbody').children().not(".child");
+        let trs = $(this.table).find('>tbody').children().not(".child");
 
         if(this.oldTrs && this.oldTrs.length == 0) {
         	// have no data for last view, there will be a tr show "No data..."
@@ -179,4 +277,4 @@ class UilaTableDirectiveWorker extends laygoon.util.BaseNgClass {
 	    return $.grep(oldTrs, function(x) {return $.inArray(x, newTrs) < 0})
 	} 
 }
-UilaTableDirectiveWorker.inject(['$timeout', '$q', '$compile', '$scope', '$sce']);
+UilaTableDirectiveWorker.inject(['$timeout', '$q', '$compile', '$scope']);
